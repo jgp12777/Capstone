636c14cb9b89f872f2513f12f6899f2fddec0e02
MadMuddimus <max.mudd@live.com>
2025-12-02 22:24:59 -0700

    Secure WebSocket

--- local-websocket/local_server.js
+++ local-websocket/local_server.js
@@ -1,10 +1,25 @@
-// Base code from ChatGPT & Donny Wals
+// Code created from ChatGPT & Donny Wals
 import { WebSocketServer } from "ws";
 
-// Create server listening on port 8080 (CHANGE PORT TO SOMETHING NO ONE WILL BE USING)
+// Create server listening on port 8080
 const wss = new WebSocketServer({port: 8080})
 
 // Handle connections
-wss.on("connection", (socket) => {
-    // send and receive messages
+wss.on('connection', function connection(wss) {
+    wss.on('message', function message(data) {
+        console.log('received %s', data);
+        wss.close();
+    });
+
+    wss.send('connection received');
+
+    var t = setInterval(function() {
+        console.log("sending message");
+        wss.send('sending message!');
+    }, 1000);
+
+    wss.on('close', function close() {
+        console.log("received close");
+        clearInterval(t);
+      });
 });
